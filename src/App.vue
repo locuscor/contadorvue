69cdce0b3380a14309c85342e34eaffaf16bc260
locuscor <locuscor@gmail.com>
2023-10-31 12:47:56 -0600

    Vue Project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from "vue";
+var counter = ref(0);
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ counter }}</h1>
+    <button @click="--counter">-</button>
+    <button @click="++counter">+</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
